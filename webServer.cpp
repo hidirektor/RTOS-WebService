44aaa861e894f0611bf28a00ab3097971e7945d0
Halil İbrahim Direktör <hidirektor@gmail.com>
2022-12-23 20:11:19 +0300

    OOP Principles added :))

--- webServer.cpp
+++ webServer.cpp
@@ -5,61 +5,65 @@
 #define STACKSIZE 2048
 #define PRIORITY 7
 
-struct http_response {
-    const char *payload;
-    size_t payload_len;
+class WebServer {
+ public:
+  WebServer() {
+    sock_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock_ < 0) {
+      printk("Error creating socket\n");
+      return;
+    }
+
+    server_addr_.sin_family = AF_INET;
+    server_addr_.sin_port = htons(80);
+    server_addr_.sin_addr.s_addr = INADDR_ANY;
+
+    int ret = bind(sock_, (struct sockaddr *)&server_addr_, sizeof(server_addr_));
+    if (ret < 0) {
+      printk("Error binding socket\n");
+      return;
+    }
+
+    ret = listen(sock_, 1);
+    if (ret < 0) {
+      printk("Error listening on socket\n");
+      return;
+    }
+  }
+
+  void run() {
+    while (1) {
+      int new_sock = accept(sock_, NULL, NULL);
+      if (new_sock < 0) {
+        printk("Error accepting connection\n");
+        return;
+      }
+
+      struct http_response res = {
+        .payload = "Hello World!",
+        .payload_len = sizeof("Hello World!") - 1
+      };
+
+      int ret = send(new_sock, &res, sizeof(res), 0);
+      if (ret < 0) {
+        printk("Error sending response\n");
+      }
+
+      close(new_sock);
+    }
+  }
+
+ private:
+  int sock_;
+  struct sockaddr_in server_addr_;
 };
 
-static char __stack http_stack[STACKSIZE];
+K_THREAD_DEFINE(http_id, STACKSIZE, WebServer::run, NULL, NULL, NULL,
+                PRIORITY, 0, K_NO_WAIT);
 
-static void http_server(void)
-{
-    struct sockaddr_in server_addr;
-    int sock, new_sock;
-    int ret;
+int main() {
+  WebServer server;
+  server.run();
 
-    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock < 0) {
-        printk("Error creating socket\n");
-        return;
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(80);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    ret = bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (ret < 0) {
-        printk("Error binding socket\n");
-        return;
-    }
-
-    ret = listen(sock, 1);
-    if (ret < 0) {
-        printk("Error listening on socket\n");
-        return;
-    }
-
-    while (1) {
-        new_sock = accept(sock, NULL, NULL);
-        if (new_sock < 0) {
-            printk("Error accepting connection\n");
-            return;
-        }
-
-        struct http_response res = {
-                .payload = "Hello World!",
-                .payload_len = sizeof("Hello World!") - 1
-        };
-
-        ret = send(new_sock, &res, sizeof(res), 0);
-        if (ret < 0) {
-            printk("Error sending response\n");
-        }
-
-        close(new_sock);
-    }
+  return 0;
 }
-
-K_THREAD_DEFINE(http_id, STACKSIZE, http_server, NULL, NULL, NULL,
-PRIORITY, 0, K_NO_WAIT);
